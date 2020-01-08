35eab399dc9c52d25fbf73b6dca706fb3d586305
flyingfive <icaman7515@hotmail.com>
2020-01-08 13:45:06 +0800

    命名

--- FlyingFive/Utils/SnowflakeId.cs
+++ FlyingFive/Utils/SnowflakeId.cs
@@ -9,7 +9,7 @@ namespace FlyingFive.Utils
     /// <summary>
     /// 动态生产有规律的ID
     /// </summary>
-    public class SnowflakeId //: IIdentifierGenerator
+    public class SnowflakeId
     {
         private readonly long _machineId = 0L;//机器ID
         private readonly long _datacenterId = 0L;//数据ID
@@ -17,44 +17,47 @@ namespace FlyingFive.Utils
         /// <summary>
         /// 唯一时间随机量
         /// </summary>
-        private static readonly long twepoch = 687888001020L;
+        private static readonly long _twepoch = 687888001020L;
         /// <summary>
         /// 机器码字节数
         /// </summary>
-        public static readonly long MachineIdBits = 5L;
+        private static readonly int _machineIdBits = 5;
         /// <summary>
         /// 数据字节数
         /// </summary>
-        public static readonly long DataCenterIdBits = 5L;
-        /// <summary>
-        /// 最大机器ID
-        /// </summary>
-        public static readonly long MaxMachineId = -1L ^ -1L << (int)MachineIdBits;
-        /// <summary>
-        /// 最大数据ID
-        /// </summary>
-        public static readonly long MaxDataCenterId = -1L ^ (-1L << (int)DataCenterIdBits);
+        private static readonly int _dataCenterIdBits = 5;
         /// <summary>
         /// 计数器字节数，12个字节用来保存计数码  
         /// </summary>
-        private static long sequenceBits = 12L;
+        private static readonly int _sequenceBits = 12;
+        /// <summary>
+        /// 最大机器ID
+        /// </summary>
+        private static readonly long _maxMachineId = -1L ^ -1L << _machineIdBits;
+        /// <summary>
+        /// 最大数据ID
+        /// </summary>
+        private static readonly long _maxDataCenterId = -1L ^ (-1L << _dataCenterIdBits);
         /// <summary>
         /// 机器码数据左移位数，就是后面计数器占用的位数
         /// </summary>
-        private static long machineIdShift = sequenceBits;
-        private static long datacenterIdShift = sequenceBits + MachineIdBits;
+        private static readonly int _machineIdShift = _sequenceBits;
+        /// <summary>
+        /// 数据标识id向左移17位(12+5) 
+        /// </summary>
+        private static readonly int _datacenterIdShift = _sequenceBits + _machineIdBits;
         /// <summary>
         /// 时间戳左移动位数就是机器码+计数器总字节数+数据字节数
         /// </summary>
-        private static long timestampLeftShift = sequenceBits + MachineIdBits + DataCenterIdBits;
+        private static readonly int _timestampLeftShift = _sequenceBits + _machineIdBits + _dataCenterIdBits;
         /// <summary>
         /// 一微秒内可以产生计数，如果达到该值则等到下一微妙在进行生成
         /// </summary>
-        private static long sequenceMask = -1L ^ -1L << (int)sequenceBits;
+        private static long _sequenceMask = -1L ^ -1L << _sequenceBits;
         /// <summary>
         /// 最后时间戳
         /// </summary>
-        private static long lastTimestamp = -1L;
+        private static long _lastTimestamp = -1L;
 
         private static object _syncObj = new object();
 
@@ -88,7 +91,7 @@ namespace FlyingFive.Utils
         {
             if (machineId >= 0)
             {
-                if (machineId > MaxMachineId)
+                if (machineId > _maxMachineId)
                 {
                     throw new ArgumentOutOfRangeException("机器码ID非法");
                 }
@@ -96,7 +99,7 @@ namespace FlyingFive.Utils
             }
             if (datacenterId >= 0)
             {
-                if (datacenterId > MaxDataCenterId)
+                if (datacenterId > _maxDataCenterId)
                 {
                     throw new ArgumentOutOfRangeException("数据中心ID非法");
                 }
@@ -104,13 +107,15 @@ namespace FlyingFive.Utils
             }
         }
 
+        private static readonly DateTime _startTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
         /// <summary>
         /// 生成当前时间戳
         /// </summary>
         /// <returns>毫秒</returns>
         private static long GetTimestamp()
         {
-            return (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+            return (long)(DateTime.UtcNow - _startTime).TotalMilliseconds;
         }
 
         /// <summary>
@@ -136,6 +141,30 @@ namespace FlyingFive.Utils
         }
 
         /// <summary>
+        /// 解析雪花ID
+        /// </summary>
+        /// <returns></returns>
+        public static string AnalyzeId(long id)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var timestamp = id >> (int)_timestampLeftShift;
+            var time = _startTime.AddMilliseconds(timestamp + _twepoch);
+            sb.Append(time.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff"));
+
+            var datacenterId = (id ^ (timestamp << _timestampLeftShift)) >> _datacenterIdShift;
+            sb.Append("_" + datacenterId);
+
+            var workerId = (id ^ ((timestamp << _timestampLeftShift) | (datacenterId << _datacenterIdShift))) >> _machineIdShift;
+            sb.Append("_" + workerId);
+
+            var sequence = id & _sequenceMask;
+            sb.Append("_" + sequence);
+
+            return sb.ToString();
+        }
+
+        /// <summary>
         /// 获取长整形的ID
         /// </summary>
         /// <returns></returns>
@@ -144,13 +173,13 @@ namespace FlyingFive.Utils
             lock (_syncObj)
             {
                 var timestamp = GetTimestamp();
-                if (SnowflakeId.lastTimestamp == timestamp)
+                if (SnowflakeId._lastTimestamp == timestamp)
                 { //同一微妙中生成ID
-                    _sequence = (_sequence + 1) & sequenceMask; //用&运算计算该微秒内产生的计数是否已经到达上限
+                    _sequence = (_sequence + 1) & _sequenceMask; //用&运算计算该微秒内产生的计数是否已经到达上限
                     if (_sequence == 0)
                     {
                         //一微妙内产生的ID计数已达上限，等待下一微妙
-                        timestamp = GetNextTimestamp(SnowflakeId.lastTimestamp);
+                        timestamp = GetNextTimestamp(SnowflakeId._lastTimestamp);
                     }
                 }
                 else
@@ -158,14 +187,14 @@ namespace FlyingFive.Utils
                     //不同微秒生成ID
                     _sequence = 0L;
                 }
-                if (timestamp < lastTimestamp)
+                if (timestamp < _lastTimestamp)
                 {
                     throw new Exception("时间戳比上一次生成ID时时间戳还小，故异常");
                 }
-                SnowflakeId.lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
-                var id = ((timestamp - twepoch) << (int)timestampLeftShift)
-                    | (_datacenterId << (int)datacenterIdShift)
-                    | (_machineId << (int)machineIdShift)
+                SnowflakeId._lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
+                var id = ((timestamp - _twepoch) << _timestampLeftShift)
+                    | (_datacenterId << _datacenterIdShift)
+                    | (_machineId << _machineIdShift)
                     | _sequence;
                 if (id == 0)
                 {
